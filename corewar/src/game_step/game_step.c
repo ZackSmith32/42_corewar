3d9ebad998036b704d65a42a4526a80aa7d432ba
Maxwell BURSON <mburson@e1z3r6p5.42.us.org>
2017-04-04 18:58:05 -0700

    refactored game_step.c

--- corewar/src/game_step/game_step.c
+++ corewar/src/game_step/game_step.c
@@ -16,7 +16,8 @@
 ** returns number of champs alive
 */
 
-static unsigned int		champ_state(struct s_champ *champ, unsigned int count)
+static unsigned int		lives_update_return_count(struct s_champ *champ,
+															unsigned int count)
 {
 	unsigned int	i;
 	unsigned int	remaining;
@@ -51,13 +52,11 @@ static struct s_champ	*pick_winner(struct s_game *game)
 	return (game->last_live_champ);
 }
 
-
-int						game_step(struct s_game *game)
+static void				step_processes(struct s_game *game)
 {
-	struct s_process *p;
-	size_t			i;
+	struct s_process	*p;
+	size_t				i;
 
-	i = 0;
 	while ((p = (struct s_process *)ft_vecindex(game->processes, i)))
 	{
 		p->countdown--;
@@ -67,14 +66,32 @@ int						game_step(struct s_game *game)
 		}	
 		i++;
 	}
+}
+
+/*
+**	If (end of game)
+** 		> pick winner
+**	else
+**		> update cycles
+*/
+
+int						game_step(struct s_game *game)
+{
+	size_t		i;
+
+	step_processes(game);
+	i = 0;
 	game->cycles_to_death--;
 	if (game->cycles_to_death == 0)
 	{
-		kill_players(game);
-		game->cycles_max -= CYCLE_DELTA;
-		game->cycles_to_death = game->cycles_max;
-		if (champ_state(game->champs, game->champ_count) < 2)
+		if (game->cycles < CYCLE_DELTA
+			|| lives_update_return_count(game->champs, game->champ_count) < 2)
 			game->winner = pick_winner(game);
+		else
+		{
+			game->cycles_max -= CYCLE_DELTA;
+			game->cycles_to_death = game->cycles_max;
+		}
 	}
 	return (0);
 }
