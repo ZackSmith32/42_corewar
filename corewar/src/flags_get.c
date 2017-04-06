aeb4ac868c5e2271353a65f8128b697e1be93d70
Andie Phan <aphan@student.42.us.org>
2017-04-05 21:52:23 -0700

    flags_get1

--- corewar/src/flags_get.c
+++ corewar/src/flags_get.c
@@ -13,9 +13,46 @@
 #include <corewar.h>
 #include <op.h>
 
-int			flags_get(int *argc, char ***argv)
+// REMINDER: extern int16_t		g_flags;
+
+
+# define SETBIT(A,k)	(A[(k/32)] |= (1 << (k%32)))
+# define CLEARBIT(A,k)	(A[(k/32)] &= ~(1 << (k%32)))
+# define TESTBIT(A,k)	(A[(k/32)] & (1 << (k%32)))
+# define VALID_FLAGS	"dnpsv"
+# define NFLAGS			5
+
+
+static int	flag_d(int *ac, char ***av, char ***champ, struct s_game *game)
 {
-	(void)(argc);
-	(void)(argv);
+
+	//set params
+	//set g_flags
+	//increment ac av as needed
+	return (0);
+	
+	//set fail case like invalid next argv
+	return (-1);
+}
+
+int			flags_get(int *ac, char ***av, char ***champ, struct s_game *game)
+{
+	int					j;
+	char				*flags;
+	uintmax_t			func_code;
+	int					(*flag_set[NFLAGS])(int *ac, char ***av,
+											char ***champ, struct s_game *game)
+
+	flag_set = {&flag_d, &flag_n, &flag_p, &flag_s, &flagv};
+	ft_bzero(champions, sizeof(*champions) * MAX_PLAYERS);
+	ft_bzero(g_flags, sizeof(g_flags));
+	flags = VALID_FLAGS;
+	while (--(*ac) && ++(*av) && **av && (*(*av)) == '-' 
+			&& (func = flags - ft_strchr(flags, *(*av))) < NFLAGS
+			&& (*(*av)) == 0)
+	{
+		if ( -1 == (flag_set[func_code])(ac, av, champ, game))
+			//handle error
+	}
 	return (0);
 }
