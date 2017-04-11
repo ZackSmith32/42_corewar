4c653d8e4e5fa538b145fc9f4ca5add05fc41b78
Zachary Smith <z.smith32@gmail.com>
2017-04-11 10:49:22 -0700

    live operation

---
+++ corewar/src/operations/live.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   live.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/05 15:51:38 by zsmith            #+#    #+#             */
+/*   Updated: 2017/04/05 16:30:26 by zsmith           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	Live requirements:
+**		x > update lives
+**		x > update last_live_champ
+**		x > move pc
+*/
+
+int				live(struct game *game, struct s_process *process)
+{
+	char				*pc;
+	unsigned int		player_name;
+
+	pc = (char *)process->pc;
+	pc++;
+	player_name = ~(unsigned int)pc;
+	if (player_name > game->champ_count)
+		return (-1);
+	(game->champs[player_name]).lives++;
+	game->last_live_champ = game->champs[player_name] 
+	move_pc(&game->arena, &process->pc, 5);
+	return (1);
+}
