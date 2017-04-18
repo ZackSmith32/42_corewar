025211f28c3c6ad2d7a7f7310c7bbf843a787a02
Zachary Smith <z.smith32@gmail.com>
2017-04-17 20:14:32 -0700

    changed byte_count to byte_index... though I'm second guessing that

--- src_corewar/src/operations/live.c
+++ src_corewar/src/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/05 15:51:38 by zsmith            #+#    #+#             */
-/*   Updated: 2017/04/17 18:39:05 by zsmith           ###   ########.fr       */
+/*   Updated: 2017/04/17 20:13:15 by zsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,24 +40,23 @@ int		live(struct s_game *game, struct s_process *process)
 int		ld(struct s_game *game, struct s_process *process)
 {
 	struct s_parameter	params[g_op_tab[2].argc]; // parse params doens't know how many params to get don't want to error out.
-	uint8_t				byte_count;
+	uint8_t				byte_index;
 	uint64_t			number_to_store;
 
 	byte_index = 0;
 
-	if (-1 == parse_and_validate_parameters(process, params, &byte_count))
+	if (-1 == parse_and_validate_parameters(process, params, &byte_index))
 		return (-1);
 
-	number_to_store = params[0]->param_val % IDX_MOD;
+	number_to_store = params[0].param_val.val % IDX_MOD;
 
-	if (params[1]->param_val < REG_NUMBER && params[1]->param_val != 0)
-		process->registors[params[1]->param_val] = number_to_store;
+	if (params[1].param_val.val < REG_NUMBER && params[1].param_val.val != 0)
+		process->registors[params[1].param_val.val] = number_to_store;
 	else
 		return (-1);
 	
 	move_pc(game->arena, &process->pc, 5);
 	return (0);
-	// ft_memmove(process->registors[(unit_8)args[1]->reg], (uint64)args[0]->val);
 }
 
 
