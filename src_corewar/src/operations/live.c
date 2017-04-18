0593aea9635f226391be2e9eda97625b6ac9896e
Zachary Smith <z.smith32@gmail.com>
2017-04-18 12:37:08 -0700

    updated ld, set prototype that registers will be stored in big endian like the board

--- src_corewar/src/operations/live.c
+++ src_corewar/src/operations/live.c
@@ -34,32 +34,39 @@ int		live(struct s_game *game, struct s_process *process)
 }
 
 /*
-**	TODO : !! change what is being stored for indirect
-**		 : registers are big endian : because it's more right
+**	TODO : store everything in big endian, convert when necessary
+**		 : x !! change what is being stored for indirect
+**		 : x registers are big endian : because it's more right
 **		 : test what happens when the register is over written with an incorrect
 **		 	value
-**		 : do we need the union? a: yes.  storing register, not what is in the register
+**		 : x do we need the union? a: yes.  storing register, not what is in the register
+**		 : what about negative numbers?
 */
 
 int8_t		ld(struct s_game *game, struct s_process *process)
 {
 	struct s_parameter	params[g_op_tab[2].argc]; // parse params doens't know how many params to get don't want to error out.
 	uint8_t				byte_offset;
-	uint64_t			number_to_store;
-
-	byte_offset = 0;
+	uint16_t			ind_offset;
+	uint8_t				reg;
 
 	if (-1 == parse_and_validate_parameters(process, params, &byte_offset))
 		return (-1);
-
-	number_to_store = params[0].param_val.val % IDX_MOD;
-
-	if (params[1].param_val.val < REG_NUMBER)
-		ft_memmove(process->registors[params[1].param_val.val], 
-			number_to_store, REG_SIZE);
+	if (params[1].param_val.val < REG_NUMBER) //  should it be '<=' ?
+		reg = params[1].param_val.val;
 	else
 		return (-1);
-	
+	if (params[0].param_type = T_DIR)
+	{
+		ft_memmove_corewar(params[0].param_val.arr,
+			process->registors[reg], REG_SIZE);
+	} 
+	else if (params[0].param_type = T_IND)
+	{
+		read_vm(params[0].param_val.val, IND_SIZE, &ind_offset);
+		ft_memmove_corewar(mask_pc(process->pc, ind_offset),
+			process->registors[reg], REG_SIZE);
+	}
 	printf("in : ld : move pc forward %d\n", byte_index);
 	move_pc(game->arena, &process->pc, 5);
 	return (0);
