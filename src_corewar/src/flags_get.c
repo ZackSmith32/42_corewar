bd154f7dd7f67d37fab70ef022a4657e43394bc5
Andie Phan <aphan@student.42.us.org>
2017-04-17 18:05:39 -0700

    flags normed

--- src_corewar/src/flags_get.c
+++ src_corewar/src/flags_get.c
@@ -12,70 +12,6 @@
 
 #include <corewar.h>
 
-static int	flag_d(char ***av, char **champ)
-{
-	(void)champ;
-	if (++(*av) && **av && ***av >= '0' && ***av <= '9')
-	{
-		g_flags.cycle_to_dump_exit = ft_atoui(**av); //set params
-		g_flags.list |= FLAG_D; //set g_flags
-		return (0);
-	}
-	return (-1);
-}
-
-static int	flag_n(char ***av, char **champ)
-{
-	unsigned int	i;
-
-	i = 0;
-	if (++(*av) && **av && ***av >= '0' && ***av <= '9'
-			&& MAX_PLAYERS > (i = ft_atoui(**av) - 1) && champ[i] == NULL
-			&& ++(*av) && **av)
-	{
-		champ[i] = **av;
-		g_flags.list |= FLAG_N;
-		return (0);
-	}
-	return (-1);
-}
-
-static int	flag_p(char ***av, char **champ)
-{
-	(void)av;
-	(void)champ;
-	g_flags.list |= FLAG_P;
-	return (0);
-}
-
-static int	flag_s(char ***av, char **champ)
-{
-	(void)champ;
-	if (++(*av) && **av && ***av >= '0' && ***av <= '9')
-	{
-		g_flags.cycle_intervals_to_dump = ft_atoui(**av);
-		g_flags.list |= FLAG_S;
-		return (0);
-	}
-	return (-1);
-}
-
-static int	flag_v(char ***av, char **champ)
-{
-	unsigned int	hex;
-
-	(void)champ;
-	hex = 0;
-	if (++(*av) && **av && ***av >= '0' && ***av <= '9'
-			&& 31 >= (hex = ft_atoui(**av)))
-	{
-		g_flags.verbosity_level |= hex;
-		g_flags.list |= FLAG_V;
-		return (0);
-	}
-	return (-1);
-}
-
 static int	flag_f(char ***av, char **champ)
 {
 	(void)av;
@@ -90,7 +26,7 @@ static void	print_flag_status(void)
 
 	count++;
 	ft_printf("flag loop #%d\n", count);
-	ft_printf("list %s\nbool %d%d%d%d%d%d\n", VALID_FLAGS, 
+	ft_printf("list %s\nbool %d%d%d%d%d%d\n", VALID_FLAGS,
 			g_flags.list & FLAG_D ? 1 : 0,
 			g_flags.list & FLAG_N ? 1 : 0,
 			g_flags.list & FLAG_P ? 1 : 0,
@@ -124,18 +60,23 @@ static int	fill_champ(char ***av, char **champ)
 	return (0);
 }
 
-int			flags_get(char ***av, char **champ)
+static void	fill_flag_set(int (*flag_set[NFLAGS])(char ***av, char **champ))
 {
-	char		*flags;
-	uintmax_t	func_code;
-	int			(*flag_set[NFLAGS])(char ***av, char **champ);
-
 	flag_set[0] = &flag_d;
 	flag_set[1] = &flag_n;
 	flag_set[2] = &flag_p;
 	flag_set[3] = &flag_s;
 	flag_set[4] = &flag_v;
 	flag_set[5] = &flag_f;
+}
+
+int			flags_get(char ***av, char **champ)
+{
+	char		*flags;
+	uintmax_t	func_code;
+	int			(*flag_set[NFLAGS])(char ***av, char **champ);
+
+	fill_flag_set(flag_set);
 	ft_bzero(champ, sizeof(*champ) * MAX_PLAYERS + 1);
 	ft_bzero(&g_flags, sizeof(g_flags));
 	flags = VALID_FLAGS;
@@ -143,15 +84,12 @@ int			flags_get(char ***av, char **champ)
 	{
 		if ((func_code = ft_strchr(flags, ***av) - flags) < NFLAGS
 				&& ++(**av) && ***av == 0
-				&& -1 == (flag_set[func_code])(av, champ))
-		{
-			g_error = 3;
+				&& -1 == (flag_set[func_code])(av, champ) && (g_error = 3))
 			return (-1);
-		}
 		if (g_flags.flag_test)
 			print_flag_status();
 	}
 	if (**av && -1 == fill_champ(av, champ) && (g_error = 3))
-			return (-1);
+		return (-1);
 	return (0);
 }
