c6ee69120daa89735af17b2758c6f2842f0bb5c8
kdavis <kyweda@gmail.com>
2017-04-20 23:15:38 -0700

    Add new_label (indexes labels)

---
+++ src_asm/src/read_body/new_label.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   new_label.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/20 22:48:30 by kdavis            #+#    #+#             */
+/*   Updated: 2017/04/20 23:14:27 by kdavis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <asm.h>
+
+/*
+** new_label takes a label and adds it to a list of labels if it is not there
+** already. Sets the labels name and address.
+*/
+
+int	new_label(t_vec *labels, char *name, int address)
+{
+	t_label	*current;
+	t_label	new;
+	size_t	i;
+
+	if (!(labels->arr))
+		if (!(ft_initialize_vec(labels, sizeof(t_label), 10, 0)))
+			return (-1);;
+	i = 0;
+	while (i < labels->len)
+	{
+		current = ft_vecindex(labels, i);
+		if (ft_strcmp(name, current->name) == 0)
+			return (1);
+	}
+	new.name = name;
+	new.address = address;
+	if (!(ft_vecapp(labels, &new, sizeof(new))))
+		return (-1);
+	return (0);
+}
--- src_asm/src/read_body/parse_label.c
+++ src_asm/src/read_body/parse_label.c
@@ -6,7 +6,7 @@
 /*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 21:52:37 by kdavis            #+#    #+#             */
-/*   Updated: 2017/04/20 22:29:24 by kdavis           ###   ########.fr       */
+/*   Updated: 2017/04/20 23:13:40 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 /*
 ** Parse_labels
+**	Determine if a new label needs to be added to the list
+**	Call the appropriate function
 */
 
 int	parse_label(char *cp, t_asm *as)
 {
-	char *label;
+	char 	*label;
+	int		ern;
 
 	if (!(label = dup_label(&cp, &as->pi.col)))
 		return (LEXICAL);
+	if (*cp == LABEL_CHAR)
+	{
+		if ((ern = new_label(label, as)))
+			ft_strdel(&label);
+	}
 	ft_printf("label:%s\ncp:%c row:%d col:%d\n",
 			label, *cp, as->pi.row, as->pi.col);
 	return (0);
