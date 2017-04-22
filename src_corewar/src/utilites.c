d5d83d1d962f6c5c20f08c9e7860e6b748aa00bb
Maxwell BURSON <mburson@e1z2r3p25.42.us.org>
2017-04-22 09:44:34 -0700

    forgot to add a file

---
+++ src_corewar/src/utilites.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/10/04 11:43:01 by zaz               #+#    #+#             */
+/*   Updated: 2017/04/13 14:56:21 by kdavis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <corewar.h>
+
+void				change_end(void *val, size_t size)
+{
+	uint8_t swap;
+	uint8_t *start;
+	uint8_t	*end;
+
+	start = (uint8_t*)val;
+	end = start + size - 1;
+	while (end > start)
+	{
+		swap = *start;
+		*start = *end;
+		*end = swap;
+		end--;
+		start++;
+	}
+}
