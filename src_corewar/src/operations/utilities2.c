1f60a996ccaa67d25196ec7bac78cf51c42b42b4
Maxwell BURSON <mburson@e1z2r3p14.42.us.org>
2017-04-24 15:46:27 -0700

    aff operation now appends its chars to aff_out

--- src_corewar/src/operations/utilities2.c
+++ src_corewar/src/operations/utilities2.c
@@ -9,8 +9,8 @@ int		check_registors(uint8_t op_code, struct s_parameter *params)
 	i = 0;
 	while (i < g_op_tab[op_code].argc)
 	{
-		if (params[i].param_type == T_REG &&
-			params[i].param_val.val >= REG_NUMBER)
+		if ((params[i].param_type == T_REG)
+			&& params[i].param_val.val >= REG_NUMBER)
 			return (-1);
 		i++;
 	}
