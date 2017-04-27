012048a655fcb3b8608a0f930e7f9e4ccde86ac5
Zachary Smith <z.smith32@gmail.com>
2017-04-26 21:24:06 -0700

    made champion pork, debuggin ldi

--- src_corewar/src/operations/ldi_lldi.c
+++ src_corewar/src/operations/ldi_lldi.c
@@ -39,9 +39,9 @@ int		ldi(struct s_game *game, struct s_process *process)
 		process->pc = pc_temp;
 		return (0);
 	}
-	offset = calc_ldi_offset(process, params); //this offset is getting set to the wrong value (maybe make sure the endianess is right?)
+	offset = calc_ldi_offset(process, params);
 	read_arena(game->arena, process->pc + (offset % IDX_MOD),
-		(void *)&process->registors[params[2].param_val.val - 1], REG_SIZE); //registors are not referenced starting at zero so we need to subtract 1 from the registor access val (what happens if that value is zero?)
+		(void *)&process->registors[params[2].param_val.val - 1], REG_SIZE);
 	modify_carry(process, process->registors[params[2].param_val.val - 1]);
 	process->pc = pc_temp;
 	return (0);
