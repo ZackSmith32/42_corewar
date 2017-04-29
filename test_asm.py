ae70c3db00bc54331a7db9f98f930e114344bc8d
kdavis <kyweda@gmail.com>
2017-04-29 13:29:26 -0700

    Add assembler tester

---
+++ test_asm.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+import glob
+import sys
+import os
+
+def	main(argv):
+	asm = argv[1]
+	direct = argv[2]
+	champ_list = create_champlist(argv[2])
+	for champ in champ_list:
+		os.system("./{} {}".format(asm, champ))
+
+def	create_champlist(direct):
+	layer3 = glob.glob("{}/*/*/*/*.s".format(direct))
+	layer2 = glob.glob("{}/*/*.s".format(direct)) 
+	layer1 = glob.glob("{}/*.s".format(direct)) 
+	ret = layer3 + layer1 + layer2
+#	print(ret)
+	return (ret)
+	#return (os.listdir(direct))
+	#champ_str = os.system("find {} | grep '\.s'".format(direct))
+	#return (champ_str.split())
+
+main(sys.argv)
