94c363e047789fd93ac2fe420193033139ff1081
kdavis <kyweda@gmail.com>
2017-04-30 01:44:16 -0700

    Add functionality to the corewar testing script
    	Added the ability for the corewar testing script to find the
    location of divergence and add that to the failure file. Also
    creates a diff.log file that holds the diff of the runs at
    different cycle times.

--- test_corewar.py
+++ test_corewar.py
@@ -7,18 +7,22 @@ import sys
 import os
 
 
-def main(argv):
+def main(argc, argv):
 	error = 0
-	if (len(argv) == 5):
+	if (argc >= 5):
+		find_div = False
+		if (argc > 5):
+			find_div = True
 		corev1 = argv[1]
 		corev2 = argv[2] 
 		total = int(argv[4])
 		open(corev1 + ".log","w").close()
 		open(corev2 + ".log","w").close()
 		open("Failure.log","w").close()
+		open("diff.log","w").close()
 		champ_list = create_champlist(argv[3])
 		for i in range(0, total):
-			error = error + test_corewar(corev1, corev2, champ_list, i)
+			error = error + test_corewar(corev1, corev2, champ_list, i, find_div)
 		percentage = ((total - error) * 100 / total)
 		grade = 91
 		if (percentage == 100):
@@ -36,7 +40,7 @@ def	create_champlist(direct):
 			ret.append(os.path.join(root, filenames))
 	return (ret)
 
-def test_corewar(v1, v2, champ_list, iteration):
+def test_corewar(v1, v2, champ_list, iteration, find_div):
 	nbr_player = random.randint(1, 4)
 	cycles = random.randint(1, 10000)
 	player_list = []
@@ -45,26 +49,56 @@ def test_corewar(v1, v2, champ_list, iteration):
 		player_list.append(champ_list[random.randint(0, len(champ_list) - 1)])
 		i = i + 1
 	player_list = ' '.join(player_list)
-	out1 = execute_corewar(v1, "-d", cycles, player_list, iteration)
-	out2 = execute_corewar(v2, "-d", cycles, player_list, iteration)
-	if not out1 == out2:
+	if is_diff(v1, v2, cycles, player_list):
+		fail_at = "Did not search for cycle"
+		if find_div:
+			fail_at = find_divergence(v1, v2, cycles, player_list)
 		with open("Failure.log", "a") as log:
-			log.write("\033[1mFailed Test {}:\033[0m\ncycles:{}\nplayers:{}\n\n".format(iteration, cycles, player_list))
+			log.write("\033[1mFailed Test {}:\033[0m\ncycles: {}\nplayers: {}\nFail at cycle: {}\n\n".format(iteration, cycles, player_list, fail_at))
 		print("\033[91m\033[1mTest {} failure\033[0m".format(iteration))
 		return (1)
 	return (0)
 
-def execute_corewar(corewar, flags, cycles, arguments, iteration):
+#runs corewar executable and returns a lowercase string of the output
+def run_corewar(corewar, command_line):
 	log = corewar + ".log"
-	command = "./{} {} {} {} | grep 0x >> {}".format(corewar, flags, cycles, arguments, log)
-	with open(log, "w") as header:
-		header.write("Test {}\ncycles: {}\nplayers: {}\n".format(iteration, cycles, iteration))
-#	print("Test: \033[96m{}\033[0m {}".format(iteration, command))
+	command = "./{} {} | grep 0x > {}".format(corewar, command_line, log)
 	os.system(command)
-	result = "blank string"
+	result = ""
 	with open(log, "r") as out:
 		result = out.read()
 		out.close()
 	return result.lower()
 
-main(sys.argv)
+#Checks to see if the output is different for i number of cycles returns true if they are different
+def	is_diff(v1, v2, i, player_list):
+	command = "-d {} {}".format(i, player_list)
+	out1 = run_corewar(v1, command)
+	out2 = run_corewar(v2, command)
+	if (out1 == out2):
+		return 0
+	with open("diff.log", "a") as diff:
+		diff.write("\nPlayers: {}\nCycles: {}\n".format(player_list, i))
+		os.system("diff -i {} {} >> diff.log".format(v1 + ".log", v2 + ".log"))
+	return 1
+
+
+#binary search for the cycle count that starts the divergence
+def find_divergence(v1, v2, cycles, player_list):
+	start = 0
+	end = cycles
+
+	while (start < end):
+		mid = (end - start) / 2 + start
+		check = is_diff(v1, v2, mid, player_list)
+		color = 92
+		if check == 1:
+			color = 91
+#		print("\033[{}mcheck: {} start: {} mid: {} end: {}\033[0m".format(color, check, start, mid, end)) #useful for seeing binary search in action
+		if (check == 1):
+			end = mid
+		else:
+			start = mid + 1
+	return end
+	
+main(len(sys.argv), sys.argv)
