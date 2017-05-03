3173dc810f6550a8e00b873f73cfbe5ce293f4a9
kdavis <kyweda@gmail.com>
2017-05-03 16:30:18 -0700

    Modify ld, lld, ldi, lldi, st to move pc even with invalide parameters

--- src_corewar/src/operations/ld_lld.c
+++ src_corewar/src/operations/ld_lld.c
@@ -6,7 +6,7 @@
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/05 15:51:38 by zsmith            #+#    #+#             */
-/*   Updated: 2017/05/02 20:44:17 by kdavis           ###   ########.fr       */
+/*   Updated: 2017/05/03 16:24:49 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,57 +19,49 @@
 int		ld(struct s_game *game, struct s_process *process)
 {
 	struct s_parameter	params[g_op_tab[0x02].argc];
-	uint8_t				*pc_temp;
+	uint8_t				*pc_og;
 	union u_val			ind_offset;
 
-	pc_temp = process->pc;
-	if (-1 == parse_and_validate_parameters(game, process, &pc_temp, params))
-		return (-1);
-	if (-1 == check_registors(process->op_code, params))
-	{
-		process->pc = pc_temp;
+	pc_og = process->pc;
+	if (-1 == parse_and_validate_parameters(game, process, params))
 		return (0);
-	}
+	if (-1 == check_registors(process->op_code, params))
+		return (0);
 	if (params[0].param_type == T_DIR)
 		process->registors[params[1].param_val.val - 1] =
 			params[0].param_val.val;
 	else if (params[0].param_type == T_IND)
 	{
 		reverse_bytes(params[0].param_val.arr, IND_SIZE, ind_offset.arr);
-		read_arena(game->arena, process->pc + (((t_ind)ind_offset.val) %
-			IDX_MOD), (uint8_t *)&process->
-			registors[params[1].param_val.val - 1], REG_SIZE);
+		read_arena(game->arena, pc_og + (((t_ind)ind_offset.val) % IDX_MOD),
+				(uint8_t *)&process->registors[params[1].param_val.val - 1],
+				REG_SIZE);
 	}
 	modify_carry(process, process->registors[params[1].param_val.val - 1]);
-	process->pc = pc_temp;
 	return (0);
 }
 
 int		lld(struct s_game *game, struct s_process *process)
 {
 	struct s_parameter	params[g_op_tab[0x0d].argc];
-	uint8_t				*pc_temp;
+	uint8_t				*pc_og;
 	union u_val			ind_offset;
 
-	pc_temp = process->pc;
-	if (-1 == parse_and_validate_parameters(game, process, &pc_temp, params))
-		return (-1);
-	if (-1 == check_registors(process->op_code, params))
-	{
-		process->pc = pc_temp;
+	pc_og = process->pc;
+	if (-1 == parse_and_validate_parameters(game, process, params))
 		return (0);
-	}
+	if (-1 == check_registors(process->op_code, params))
+		return (0);
 	if (params[0].param_type == T_DIR)
 		process->registors[params[1].param_val.val - 1] =
 			params[0].param_val.val;
 	else if (params[0].param_type == T_IND)
 	{
 		reverse_bytes(params[0].param_val.arr, IND_SIZE, ind_offset.arr);
-		read_arena(game->arena, process->pc + (t_ind)ind_offset.val,
+		read_arena(game->arena, pc_og + (t_ind)ind_offset.val,
 			(uint8_t *)&process->registors[params[1].param_val.val - 1],
 			REG_SIZE);
 	}
 	modify_carry(process, process->registors[params[1].param_val.val - 1]);
-	process->pc = pc_temp;
 	return (0);
 }
--- src_corewar/src/operations/live_st_zjmp.c
+++ src_corewar/src/operations/live_st_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/05 15:51:38 by zsmith            #+#    #+#             */
-/*   Updated: 2017/05/02 20:43:55 by kdavis           ###   ########.fr       */
+/*   Updated: 2017/05/03 16:28:38 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,28 +30,24 @@ int		live(struct s_game *game, struct s_process *process)
 int		st(struct s_game *game, struct s_process *process)
 {
 	struct s_parameter	params[g_op_tab[0x03].argc];
-	uint8_t				*pc_temp;
+	uint8_t				*pc_og;
 	union u_val			ind_offset;
 
-	pc_temp = process->pc;
-	if (-1 == parse_and_validate_parameters(game, process, &pc_temp, params))
+	pc_og = process->pc;
+	if (-1 == parse_and_validate_parameters(game, process, params))
 		return (-1);
 	if (-1 == check_registors(process->op_code, params))
-	{
-		process->pc = pc_temp;
 		return (0);
-	}
 	if (params[1].param_type == T_REG)
 		process->registors[params[1].param_val.val - 1] =
 			process->registors[params[0].param_val.val - 1];
 	else if (params[1].param_type == T_IND)
 	{
 		reverse_bytes(params[1].param_val.arr, IND_SIZE, ind_offset.arr);
-		write_arena(game->arena, process->pc +((t_ind)ind_offset.val % IDX_MOD),
+		write_arena(game->arena,pc_og +((t_ind)ind_offset.val % IDX_MOD),
 			(uint8_t *)&process->registors[params[0].param_val.val - 1],
 			REG_SIZE);
 	}
-	process->pc = pc_temp;
 	return (0);
 }
 
