e69c9e3f8decc757a28824b9134bff78803127c4
kdavis <kyweda@gmail.com>
2017-05-06 22:52:17 -0700

    Move ldi and lldi to seperate folder and use unique flatten function for those ops

--- src_corewar/src/operations/ldi_lldi.c
+++ src_corewar/src/operations/ldi_lldi.c
@@ -1,48 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sti.c                                              :+:      :+:    :+:   */
+/*   ldi_lldi.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aphan <aphan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/05 15:51:38 by aphan             #+#    #+#             */
-/*   Updated: 2017/05/02 20:26:51 by kdavis           ###   ########.fr       */
+/*   Created: 2017/05/06 22:44:42 by kdavis            #+#    #+#             */
+/*   Updated: 2017/05/06 22:44:45 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <corewar.h>
 
-static int			flatten(uint8_t *arena, struct s_process *process,
-						struct s_parameter *param, uint8_t *pc_og)
-{
-	if (param->param_type == T_REG)
-	{
-		if (--param->param_val.val >= REG_NUMBER)
-			return (-1);
-		param->param_val.val = process->registors[param->param_val.val];
-		change_end(param->param_val.arr, REG_SIZE);
-		param->param_val.val = (t_s_op_arg)(t_s_reg)param->param_val.val;
-	}
-	else if (param->param_type == T_IND)
-	{
-		change_end(param->param_val.arr, IND_SIZE);
-		param->param_val.val %= IDX_MOD;
-		read_arena(arena, pc_og + (t_ind)param->param_val.val,
-			param->param_val.arr, DIR_SIZE);
-		change_end(param->param_val.arr, DIR_SIZE);
-		param->param_val.val = (t_s_op_arg)(t_dir)param->param_val.val;
-	}
-	else if (param->param_type == T_DIR)
-	{
-		change_end(param->param_val.arr, IND_SIZE);
-		param->param_val.val = (t_s_op_arg)(t_ind)param->param_val.val;
-	}
-	return (0);
-}
-
-//TODO: is l_flatten needed?
 static int			l_flatten(uint8_t *arena, struct s_process *process,
-						struct s_parameter *param, uint8_t *pc_og)
+						struct s_parameter *param, uint8_t pc_og[2])
 {
 	if (param->param_type == T_REG)
 	{
@@ -55,67 +26,50 @@ static int			l_flatten(uint8_t *arena, struct s_process *process,
 	else if (param->param_type == T_IND)
 	{
 		change_end(param->param_val.arr, IND_SIZE);
-		read_arena(arena, pc_og + (t_ind)param->param_val.val,
-			param->param_val.arr, DIR_SIZE);
-		change_end(param->param_val.arr, DIR_SIZE);
-		param->param_val.val = (t_s_op_arg)(t_dir)param->param_val.val;
+		if (pc_og[1])
+			param->param_val.val %= IDX_MOD;
+		read_arena(arena, pc_og[0] + (t_ind)param->param_val.val,
+			param->param_val.arr, IND_SIZE);
+		change_end(param->param_val.arr, IND_SIZE);
+		param->param_val.val = (t_s_op_arg)(t_ind)param->param_val.val;
 	}
 	else if (param->param_type == T_DIR)
 	{
 		change_end(param->param_val.arr, IND_SIZE);
 		param->param_val.val = (t_s_op_arg)(t_ind)param->param_val.val;
 	}
-	return (0);
-}
-
-int				sti(struct s_game *game, struct s_process *process)
-{
-	struct s_parameter	params[g_op_tab[11].argc];
-	uint8_t				*pc_og;
-	t_ind				offset;
-
-	pc_og = process->pc;
-	if (-1 == parse_and_validate_parameters(game, process, params))
-		return (0);
-	if (--params[0].param_val.val >= REG_NUMBER
-		|| -1 == flatten(game->arena, process, &params[1], pc_og)
-		|| -1 == flatten(game->arena, process, &params[2], pc_og))
-		return (0);
-	offset = (t_s_op_arg)(params[1].param_val.val + params[2].param_val.val)
-				% IDX_MOD;
-	write_arena(game->arena, pc_og + offset,
-		(uint8_t*)&process->registors[params[0].param_val.val], REG_SIZE);
 	return (0);
 }
 
 int				ldi(struct s_game *game, struct s_process *process)
 {
 	struct s_parameter	params[g_op_tab[11].argc];
-	uint8_t				*pc_og;
+	uint8_t				pc_og[2];
 	t_ind				offset;
 
-	pc_og = process->pc;
+	pc_og[0] = process->pc;
+	pc_og[1] = 1;
 	if (-1 == parse_and_validate_parameters(game, process, params))
 		return (0);
 	if (--params[2].param_val.val >= REG_NUMBER
-		|| -1 == flatten(game->arena, process, &params[0], pc_og)
-		|| -1 == flatten(game->arena, process, &params[1], pc_og))
+		|| -1 == l_flatten(game->arena, process, &params[0], pc_og)
+		|| -1 == l_flatten(game->arena, process, &params[1], pc_og))
 		return (0);
 	offset = (t_s_op_arg)(params[0].param_val.val + params[1].param_val.val)
 				% IDX_MOD;
 	read_arena(game->arena, pc_og + offset,
 		(uint8_t*)&process->registors[params[2].param_val.val], REG_SIZE);
-//	process->carry = process->registors[params[2].param_val.val] == 0;
 	return (0);
 }
 
 int				lldi(struct s_game *game, struct s_process *process)
 {
 	struct s_parameter	params[g_op_tab[11].argc];
-	uint8_t				*pc_og;
+	uint8_t				pc_og[2];
 	t_ind				offset;
 
-	pc_og = process->pc;
+	pc_og[0] = process->pc;
+	pc_og[1] = 0;
 	if (-1 == parse_and_validate_parameters(game, process, params))
 		return (0);
 	if (--params[2].param_val.val >= REG_NUMBER
---
+++ src_corewar/src/operations/sti.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sti.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aphan <aphan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/05 15:51:38 by aphan             #+#    #+#             */
+/*   Updated: 2017/05/02 20:26:51 by kdavis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <corewar.h>
+
+static int			flatten(uint8_t *arena, struct s_process *process,
+						struct s_parameter *param, uint8_t *pc_og)
+{
+	if (param->param_type == T_REG)
+	{
+		if (--param->param_val.val >= REG_NUMBER)
+			return (-1);
+		param->param_val.val = process->registors[param->param_val.val];
+		change_end(param->param_val.arr, REG_SIZE);
+		param->param_val.val = (t_s_op_arg)(t_s_reg)param->param_val.val;
+	}
+	else if (param->param_type == T_IND)
+	{
+		change_end(param->param_val.arr, IND_SIZE);
+		param->param_val.val %= IDX_MOD;
+		read_arena(arena, pc_og + (t_ind)param->param_val.val,
+			param->param_val.arr, DIR_SIZE);
+		change_end(param->param_val.arr, DIR_SIZE);
+		param->param_val.val = (t_s_op_arg)(t_dir)param->param_val.val;
+	}
+	else if (param->param_type == T_DIR)
+	{
+		change_end(param->param_val.arr, IND_SIZE);
+		param->param_val.val = (t_s_op_arg)(t_ind)param->param_val.val;
+	}
+	return (0);
+}
+
+int				sti(struct s_game *game, struct s_process *process)
+{
+	struct s_parameter	params[g_op_tab[11].argc];
+	uint8_t				*pc_og;
+	t_ind				offset;
+
+	pc_og = process->pc;
+	if (-1 == parse_and_validate_parameters(game, process, params))
+		return (0);
+	if (--params[0].param_val.val >= REG_NUMBER
+		|| -1 == flatten(game->arena, process, &params[1], pc_og)
+		|| -1 == flatten(game->arena, process, &params[2], pc_og))
+		return (0);
+	offset = (t_s_op_arg)(params[1].param_val.val + params[2].param_val.val)
+				% IDX_MOD;
+	write_arena(game->arena, pc_og + offset,
+		(uint8_t*)&process->registors[params[0].param_val.val], REG_SIZE);
+	return (0);
+}
